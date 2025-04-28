30c0c74ddf8ca9db6399955323548bf1b3d3e5e1
王鑫 <wangxin4@centaline.com.cn>
2025-04-28 10:13:34 +0800

    update

--- springboot-jakarta-jwt-demo/src/main/java/com/nutcracker/example/demo/web/WebUtil.java
+++ springboot-jakarta-jwt-demo/src/main/java/com/nutcracker/example/demo/web/WebUtil.java
@@ -1,11 +1,16 @@
 package com.nutcracker.example.demo.web;
 
 import com.nutcracker.common.domain.User;
+import com.nutcracker.common.util.JwtUtil;
 import com.nutcracker.example.demo.constant.DemoConstants;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
 
 /**
  * Web选项
@@ -41,6 +46,13 @@ public class WebUtil {
      */
     public static User getSessionUser(HttpServletRequest request) {
         return (User) request.getSession().getAttribute(DemoConstants.LOGIN_USER_SESSION_KEY);
+    }
+
+    public static User getUser(String secret) {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        String token = request.getHeader("token");
+        log.info("token:{}", token);
+        return JwtUtil.parseToken(token, secret);
     }
 
 }
