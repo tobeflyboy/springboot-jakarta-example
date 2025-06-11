f89ee9e8ab3c63cc92784259ecb95e1df84389b3
王鑫 <wangxin4@centaline.com.cn>
2025-06-11 14:56:39 +0800

    集成编译打包

---
+++ springboot-jakarta-example-demo/Dockermake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+build_date=`date +'%Y-%m-%dT%H:%M:%S%z' | tr -d '\n'`
+build_branch=`git branch | grep "^\*" | sed -e "s/^\*\ //"`
+build_commit=`git rev-parse HEAD | tr -d '\n'`
+build_version=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
+
+docker build --no-cache \
+  --build-arg BUILD_DATE=${build_date} \
+  --build-arg BUILD_BRANCH=${build_branch} \
+  --build-arg BUILD_COMMIT=${build_commit} \
+  --build-arg BUILD_VERSION=${build_version} \
+  -f Dockerfile -t your-company.harbor.com/admin-system/springboot-jakarta-example-demo:${build_version} .
+#docker push your-company.harbor.com/admin-system/springboot-jakarta-example-demo:${build_version}
+#docker rmi your-company.harbor.com/admin-system/springboot-jakarta-example-demo:${build_version}
